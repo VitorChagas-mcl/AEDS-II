a9d9ec04d8cc9e58cc5f4b67860ae131eb7cc532
Vitor Chagas <vitorcjagas@gmail.com>
2026-04-20 13:44:45 -0300

    Primeiras atividades de ordenação

---
+++ TPS/TP02/ordenacao/insercao/Principal.java
@@ -0,0 +1,433 @@
+import java.util.*;
+import java.io.*;
+
+class Hora{
+    private int hora;
+    private int minuto;
+    // construtor de hora
+    public Hora(int hora, int minuto){
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+    //gets e sets
+    public int getHora(){
+        return hora;
+    }
+
+    public int getMinuto(){
+        return minuto;
+    }
+
+    public void setHora(int hora){
+        this.hora = hora;
+    }
+
+    public void setMinuto(int minuto){
+        this.minuto = minuto;
+    }
+    // parse de hora 
+    public static Hora parseHora(String s){
+        Scanner scan = new Scanner(s);// scanner da string
+        scan.useDelimiter(":");// separa por :
+        int hora = scan.nextInt();//leitura dos valores
+        int minuto = scan.nextInt();
+        Hora h = new Hora(hora,minuto);// criar o objeto hora
+        return h;// retona a nova hora
+    }
+
+    public String formatar(){
+        return String.format("%02d:%02d", this.hora, this.minuto); // retorna a hora formatada
+    }
+}   
+
+class Data{
+    
+    private int ano;
+    private int mes;
+    private int dia;
+    // construtor de data
+    public Data(int ano, int mes, int dia){
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+    }
+    //gets e sets
+    public int getAno(){
+        return ano;
+    }
+
+    public int getMes(){
+        return mes;
+    }
+
+    public int getDia(){
+        return dia;
+    }
+
+    public void setAno(int ano){
+        this.ano = ano;
+    }
+
+    public void setMes(int mes){
+        this.mes = mes;
+    }
+
+    public void setDia(int dia){
+        this.dia = dia;
+    }
+    // parse de data 
+    public static Data parseData(String s){ 
+        Scanner scan = new Scanner(s);// scanner da string 
+        scan.useDelimiter("-");// delimitacao pro -
+        int ano = scan.nextInt();//leitura dos valores
+        int mes = scan.nextInt();
+        int dia = scan.nextInt();
+        scan.close();
+        Data data = new Data(ano, mes, dia);// cria um objeto data
+        return data;// retorna a nova data
+    }
+   
+    public String formatar(){
+        return String.format("%02d/%02d/%04d", this.dia, this.mes, this.ano);// retorna a data no padrao brasileiro
+    }
+}
+    
+class Restaurante{
+    private int idRestaurante;
+    private String nome;
+    private String cidade;
+    private int capacidade;
+    private double avaliacao;
+    private String[] tiposCozinha;
+    private int faixa_preco;
+    private Hora horarioAbertura;
+    private Hora horarioFechamento;
+    private Data dataAbertura;
+    private boolean aberto;
+    //construtor de restaurante
+    public Restaurante(int idRestaurante, String nome, String cidade, int capacidade, double avaliacao,
+            String[] tiposCozinha,int faixa_preco, Hora horarioAbertura, Hora horarioFechamento, Data dataAbertura, boolean aberto) {
+        this.idRestaurante = idRestaurante;
+        this.nome = nome;
+        this.cidade = cidade;
+        this.capacidade = capacidade;
+        this.avaliacao = avaliacao;
+        this.tiposCozinha = tiposCozinha;
+        this.faixa_preco = faixa_preco;
+        this.horarioAbertura = horarioAbertura;
+        this.horarioFechamento = horarioFechamento;
+        this.dataAbertura = dataAbertura;
+        this.aberto = aberto;
+    }
+    //gets e sets
+    public int getIdRestaurante() {
+        return idRestaurante;
+    }
+
+    public void setIdRestaurante(int idRestaurante) {
+        this.idRestaurante = idRestaurante;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public String[] getTiposCozinha() {
+        return tiposCozinha;
+    }
+
+    public void setTiposCozinha(String[] tiposCozinha) {
+        this.tiposCozinha = tiposCozinha;
+    }
+
+    public int getFaixa_Preco(){
+        return faixa_preco;
+    }
+
+    public void setFaixa_Preco(int faixa_preco){
+        this.faixa_preco = faixa_preco;
+    }
+
+    public Hora getHorarioAbertura() {
+        return horarioAbertura;
+    }
+
+    public void setHorarioAbertura(Hora horarioAbertura) {
+        this.horarioAbertura = horarioAbertura;
+    }
+
+    public Hora getHorarioFechamento() {
+        return horarioFechamento;
+    }
+
+    public void setHorarioFechamento(Hora horarioFechamento) {
+        this.horarioFechamento = horarioFechamento;
+    }
+
+    public Data getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(Data dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+    //função de pegar a quantidade do faixa preco e passar para int
+    public static int pegarFaixa_Preco(String s){
+        int cont = 0;    
+        for(int i = 0; i < s.length(); i++)
+            if(s.charAt(i) == '$') cont++;
+
+        return cont;
+    }
+
+    public static Restaurante parseRestaurante(String s){
+        Scanner scan = new Scanner(s);
+        scan.useLocale(Locale.US);// usei para ler o double, pois vem na string ex:4.4 
+        scan.useDelimiter(",");// delimitei para pegar ate a ,
+        int id = scan.nextInt();
+        String nome = scan.next();
+        String cidade = scan.next();
+        int capacidade = scan.nextInt();
+        double avaliacao = scan.nextDouble();
+        String tpCozinha = scan.next();  
+        int faixa_preco = pegarFaixa_Preco(scan.next());
+        String horarios = scan.next();
+        Scanner scanHorarios = new Scanner(horarios); // novo scanner para ler as horas
+        scanHorarios.useDelimiter("-");// delimitei para pegar ate - 
+        Hora horaAbertura = Hora.parseHora(scanHorarios.next()); // chama da funcao para o parseHora
+        Hora horaFechamento = Hora.parseHora(scanHorarios.next());
+        scanHorarios.close(); // fechamento do scanner de horas
+        Data dataAbertura = Data.parseData(scan.next()); // chamada da funcao para o parseData
+        String abertoStr = scan.next(); // pega a string true ou false
+        boolean aberto = (abertoStr.compareTo("true") == 0); // verifica se a palavra é true ou false e restona o seu valor
+
+        scan.close(); // fachamento do scan principal
+
+        String[] aux = new String[10]; // variavel auxiliar para pegar os Tipos de cozinha
+        int cout = 0;// contador para pegar o tamanho exato de tipos
+        Scanner scanTp = new Scanner(tpCozinha); // novo scanner de tuois
+        scanTp.useDelimiter(";");// delimitei o ; pois ele separa os restaurantes
+        while(scanTp.hasNext()){// verifica se exite palavra
+            String palavra = scanTp.next();// le a palavra do vetor de string
+            if(palavra.length() > 0){// se o tamanho da palavra for maior que 0
+                aux[cout] = palavra; // auxiliar recebe a palavra adiciona a posição
+                cout++;// e contador receber + 1
+            }
+        }
+        scanTp.close();// fecho o scanner de tipos
+    
+        String[] tipoCozinha = new String[cout]; // crio um novo vetor de string
+        for(int i = 0; i < cout; i++){// pego a quantidade exata de tipos
+            tipoCozinha[i] = aux[i];// adiciono no vetor de tipoCozinha
+        }
+        //retorno um objeto restaurante
+        return new Restaurante(id, nome, cidade, capacidade, avaliacao, tipoCozinha, 
+                               faixa_preco ,horaAbertura, horaFechamento, dataAbertura, aberto);
+    }
+
+    public String formatar(){
+     
+        String strCozinhas = "";// criei um vetor para formatar o tipo de cozinha
+        for(int i = 0; i < tiposCozinha.length; i++){// pego o tamanho exato do vetor
+            strCozinhas += tiposCozinha[i];//adiciono a string o tipo de cozinha
+            if(i < tiposCozinha.length - 1){// verifica se o i é menor que o tamanho do vetor se for adiciona ,
+                strCozinhas += ","; 
+            }
+        }
+        int n = this.faixa_preco;// pego o valor de faixa_preco
+        String faixa_p = "";//crio uma string nova
+        for(int i = 0; i < n; i++){
+            faixa_p += '$';// adiciono a quantidade de n com $
+        }
+        
+        double valor = this.avaliacao;
+        String strAvaliacao = valor + ""; // pego o valor atual do double e coloco como string
+                                         // se nao passar para string o formate retorna com , pois usar 
+                                         // a localizacao que esta no meu pc
+
+        String s = String.format("[%d ## %s ## %s ## %d ## %s ## [%s] ## %s ## %s-%s ## %s ## %b]", 
+                idRestaurante, nome, cidade, capacidade, strAvaliacao, strCozinhas, 
+                faixa_p, horarioAbertura.formatar(),horarioFechamento.formatar(), // formata o restaurante para o formato pedido
+                dataAbertura.formatar(), aberto);
+        return s; // retorna a string de restaurante formatado
+    }
+}
+
+class ColecaoRestaurantes{
+    private int tamanho;
+    private Restaurante[] restaurantes;
+    //contrutor sem parametros
+    public ColecaoRestaurantes(){
+        this.tamanho = 0;
+        this.restaurantes = null;
+    }
+    //contrutor com parametros
+    public ColecaoRestaurantes(int tamanho){
+        this.tamanho = tamanho;//pega o tamanho da colecao
+        this.restaurantes = new Restaurante[tamanho];// cria um vetor com o tamanho da colecao
+    }
+
+    public int getTamanho(){
+        return tamanho;       
+    }
+
+    public void setTamanho(int tam){
+        this.tamanho = tam;
+    }
+        
+    public Restaurante[] getRestaurantes(){
+        return restaurantes;
+    }
+
+    public void setRestaurantes(Restaurante[] restaurantes){
+        this.restaurantes = restaurantes;
+    }
+
+    public void lerCsv(String path) throws Exception{// tive que usar throws porque estava dando erro
+        File arquivo = new File(path);// abertura do arquivo para ler a linha de restaurante
+        Scanner scan = new Scanner(arquivo);//abre o scan do csv
+        
+        if(scan.hasNextLine())//pula o cabecalho
+            scan.nextLine();
+
+        int i = 0;//um contador para o indece
+        while(scan.hasNextLine()){//verifica se existe linha
+           String linha = scan.nextLine();//le a linha de restaurante
+           restaurantes[i] = Restaurante.parseRestaurante(linha);// chama a funcao do parse e guarda o restaurante 
+           i++;// incremento do indece
+        }
+        
+    }
+
+    public static ColecaoRestaurantes lerCsv() throws Exception{
+        File arquivo = new File("/tmp/restaurantes.csv"); // abertura do arquivo
+        Scanner scan = new Scanner(arquivo);//leitura do arquivo
+
+        int tam = 0;//contador para o tamanho de linha
+        while(scan.hasNext()){
+            scan.nextLine();//le a linha 
+            tam++; // Adiciona a quantidade de linha lida no arquivo csv
+        }
+
+        scan.close();
+        ColecaoRestaurantes novaCol = new ColecaoRestaurantes(tam - 1); // Cria o objeto colecao
+        novaCol.lerCsv("/tmp/restaurantes.csv");// chamada da funcao de para leitura de restaurante
+        
+        return novaCol; // retorna a colecao de restaurante 
+    }
+
+    public Restaurante buscarPorId(int id){
+         for(int i = 0; i < tamanho; i++){//pega o tamanho do vetor
+            if(restaurantes[i].getIdRestaurante() == id){// e procura o restaurante na posicao atual 
+                // e verifica se é igual ao id
+                return restaurantes[i];// restona o restaurante na posicao 
+            }
+        }
+        return null;// se nao achar retorna nada
+     }
+}
+
+public class Principal{
+     public static int mov = 0;
+     public static int comp = 0;
+
+    public static void Insercao(Restaurante[] restaurantes, int n){
+      
+        for(int i = 1; i < n; i++){
+            Restaurante aux = restaurantes[i];
+            mov++;
+            int j = i - 1;
+            
+            comp++;
+            while((j >= 0) && (aux.getCidade().compareTo(restaurantes[j].getCidade()) < 0)){
+                restaurantes[j + 1] = restaurantes[j];
+                mov++;
+                j--;
+
+                if(j >= 0) comp++;
+            }
+            mov++;
+            restaurantes[j + 1] = aux;
+        }
+    }
+
+    public static long now(){
+        return new Date().getTime();
+    }
+
+    public static void main(String[] args) throws Exception{
+            Scanner scan = new Scanner(System.in);//scaner para leitura da entrada
+            ColecaoRestaurantes cr = ColecaoRestaurantes.lerCsv();//criar o colacao restaurante e preenche
+            Restaurante[] r = new Restaurante[1000];
+            int r_ordenados = 0;
+
+            double inicio, fim, total_t;
+
+            String linha = scan.next();// le a primeira linha do pub.in
+                
+            while(linha.compareTo("-1") != 0){// verifica se é igual ao -1 para encerra
+               int id = Integer.parseInt(linha);// parse int para pegar o valor de id
+               Restaurante aux = cr.buscarPorId(id);//busca o restaurante
+               if(aux != null){//verifica se é diferente de null
+                    r[r_ordenados] = aux;
+                    r_ordenados++;
+               }
+
+               linha = scan.next();//leitura da proxima linha
+            }
+            scan.close();// fechamanto do scan
+            
+            inicio = now();
+            Insercao(r, r_ordenados);
+            fim = now();
+  
+            total_t = (fim - inicio)/1000.0;
+
+           // System.out.println("inicio: " + inicio + " " + "Fim: " + fim + " " + "Total: " + total_t);
+            for(int i = 0; i < r_ordenados; i++)
+                System.out.println(r[i].formatar());
+
+            FileWriter arq = new FileWriter("880222_insercao.txt");
+            PrintWriter gravarArq = new PrintWriter(arq);
+
+            gravarArq.printf("880222\t Comparacoes: %d\t Movimentacao: %d\t Tempo: %.2f\n", comp, mov, total_t);
+
+            gravarArq.close();
+        }
+ }
---
+++ TPS/TP02/ordenacao/selecao/Colecao.c
@@ -0,0 +1,302 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+//criei todas as structs que preciso
+typedef struct Data{
+    int dia;
+    int mes;
+    int ano;
+}Data;
+
+typedef struct Hora{
+    int hora;
+    int minuto;
+}Hora;
+
+typedef struct Restaurante{
+    int id_restaurante;
+    char* nome;
+    char* cidade;
+    int capacidade;
+    double avaliacao;
+    char** tipo_cozinha;
+    int faixa_preco;
+    Hora hora_abertura;
+    Hora hora_fechamento;
+    Data data_abertura;
+    bool aberto;
+}Restaurante;
+
+typedef struct ColecaoRestaurante{
+    int tamanho;
+    Restaurante* restaurante;
+}Colecao_Restaurante;
+
+Data parse_data(char *s){
+    Data d;
+
+    sscanf(s, "%d-%d-%d", &d.ano, &d.mes, &d.dia);//leitura de data
+    
+    return d;// retona uma struct de data
+}
+
+void formatar_data(Data* data, char* buffer){
+    sprintf(buffer,"%02d/%02d/%04d", data->dia, data->mes, data->ano);//pega a Data e passa formatada para o buffer
+}
+
+Hora parse_hora(char *s){
+    Hora h;
+    
+    sscanf(s,"%d:%d", &h.hora, &h.minuto);//ler de Hora
+
+    return h;// retorna uma struct de hora
+}
+
+void formatar_hora(Hora* hora, char* buffer){
+    sprintf(buffer, "%02d:%02d", hora->hora, hora->minuto);// pega a Hora e passa formatada para o buffer
+}
+
+void liberar_restaurante(Restaurante* r) {// criei uma funcao para liberar os vetores alocados
+    free(r->nome);
+    free(r->cidade);
+    free(r->tipo_cozinha[0]);
+    free(r->tipo_cozinha);
+}
+
+Restaurante* parse_restaurante(char *s){
+    Restaurante* r = (Restaurante*)malloc(sizeof(Restaurante));// crio um restaurante novo
+    if(r == NULL){
+        printf("Nao foi possivel criar restaurante!");
+        return NULL;
+    }
+    char hora_a[6], hora_f[6], data_a[11], nome[100], cidade[100], preco[10], tipo[40], aberto[10];// crio vetores de char para leitura da string
+
+    sscanf(s, "%d,%[^,],%[^,],%d,%lf,%[^,],%[^,],%[^-]-%[^,],%[^,],%[^\n]",//leitura da string de restaurante
+           &r->id_restaurante, nome, cidade, &r->capacidade,
+           &r->avaliacao, tipo, preco, hora_a, hora_f,
+           data_a, aberto);
+
+    r->aberto = (strcmp(aberto, "true") == 0);//verifica se o char aberto e true, se for r->aberto recebe true
+    r->hora_abertura = parse_hora(hora_a);//chama da funcao para o parse_hora
+    r->hora_fechamento = parse_hora(hora_f);// mesma chama da de cima para strings diferente
+    r->data_abertura = parse_data(data_a);// chama da funcao parse_data 
+    
+    int tam = 0;//crio uma variavel para pegar o tamanho
+    while(nome[tam] != '\0') tam++;//leio ate o \0
+    r->nome = (char*)malloc((tam + 1) * sizeof(char));// e aloco o tamanho exato do vetor e coloco + 1
+                                                     // casa para o \0
+    
+    sprintf(r->nome,"%s", nome);// passo char nome, para a char da struct
+    
+    tam = 0;// zero o tamanho
+    while(cidade[tam] != '\0') tam++;//pega o tamanho
+    r->cidade = (char*)malloc((tam + 1) * sizeof(char));//aloca
+    
+    sprintf(r->cidade,"%s", cidade);//para para a variavel da cidade na struct
+
+    tam = 0;//zero o contador
+    while(preco[tam] != '\0') tam++;//pega o tamanho
+    r->faixa_preco = tam;//passo o valor exato da quantidade de $
+
+    tam = 0;// zera o contador
+    while(tipo[tam] != '\0') tam++;//pega o valor
+    for(int i = 0; tipo[i] != '\0'; i++)//leitura ate o \0
+        if(tipo[i] == ';') tipo[i] = ',';//substitui o ; por ,
+
+    r->tipo_cozinha = (char**)malloc(1 * sizeof(char*));//cria uma posicao do char duplo
+    r->tipo_cozinha[0] = (char*)malloc((tam + 1) * sizeof(char));//aloco o tamanho da quantidade de tipos exato
+
+    sprintf(r->tipo_cozinha[0],"%s",tipo);//pega a primeira possicao e preenche com a string
+   
+
+    return r;// retorna um novo restaurante
+}
+
+void formatar_restaurante(Restaurante* restaurante, char* buffer){
+    char hora_fechamento[7], hora_abertura[7], data_abertura[12], str_aberto[6];// crio vetores de char 
+
+    formatar_hora(&restaurante->hora_abertura, hora_abertura);//passo a hora formata para o vetor
+    formatar_hora(&restaurante->hora_fechamento, hora_fechamento);//mesma coisa que a funcao de cima para vetor diferente
+    formatar_data(&restaurante->data_abertura, data_abertura);// passo a Data formatada para o veto de data
+  
+    char f_preco[5];// crio um vetor de proc
+    int i;// i para pegar o tamanho do vetor
+    for(i = 0; i < restaurante->faixa_preco; i++){// conto
+        f_preco[i] = '$';// passo na posicao o $
+    }
+    f_preco[i] = '\0';// adiciono o \0 ao final
+
+    if(restaurante->aberto == true) {// verifico se é true a variavel 
+        sprintf(str_aberto, "true");// se for adiciona true a string 
+    }else{
+        sprintf(str_aberto, "false");// se nao adiciona false
+    }
+    sprintf(buffer,"[%d ## %s ## %s ## %d ## %.1lf ## [%s] ## %s ## %s-%s ## %s ## %s]",//formatacao do restaurante
+        restaurante->id_restaurante, restaurante->nome, restaurante->cidade,
+        restaurante->capacidade, restaurante->avaliacao, restaurante->tipo_cozinha[0],
+        f_preco, hora_abertura, hora_fechamento, data_abertura, str_aberto);
+}
+
+void ler_csv_colecao(Colecao_Restaurante* colecao, char* path){
+    
+    FILE *arq = fopen("/tmp/restaurantes.csv", "r");// abre o arquivo
+
+    if(arq == NULL){//verifica se o ponteiro é null
+        printf("Erro ao abrir arquivo!");
+        return;// se for retorna nada
+    }
+    char linha[200];// pega o linha
+
+    fgets(linha, sizeof(linha), arq);//leio o cabecalho
+    
+    int i = 0;//variavel para a quantidade
+    while(fgets(linha, sizeof(linha), arq) != NULL){// leio ate o fgets retorna null
+        Restaurante* aux = parse_restaurante(linha); //Criei um aux para o parse_restaurante
+
+        colecao->restaurante[i] = *aux; //Faco as transferencia do auxilia para o restaurente na posicao i
+                                         // de restaurante
+        i++;
+        free(aux);//Libera a memoria
+    }
+
+    fclose(arq);//fecho o arquivo
+}
+
+Colecao_Restaurante* ler_csv(){//funcao para cria a colecao
+        FILE *arq = fopen("/tmp/restaurantes.csv", "r");// abro o arquivo para leitura
+
+        if(arq == NULL){// verifico se o ponteiro do arquivo nao é nulll
+            printf("Erro ao abrir arquivo!");
+            return NULL;// se for retorna null
+        }
+
+        int tam = 0;//crio uma variavel para o tamanho da colecao
+        char linha[200];
+
+        while(fgets(linha, sizeof(linha), arq) != NULL){//leio ate o final do arquivo 
+            tam++;//incremento do tam
+        }
+
+        fclose(arq);// fecho o arquivo
+
+        Colecao_Restaurante* novaCole = (Colecao_Restaurante*) malloc(sizeof(Colecao_Restaurante));// crio uma colacao
+        if(novaCole == NULL){// se a colacao nao for aloca
+            printf("Erro ao alocar Colecao!");
+            return NULL;// retorna null
+        }
+        novaCole->tamanho = tam - 1;// recebe o tamanho exata da colecao
+        novaCole->restaurante = (Restaurante*)malloc((tam - 1) * sizeof(Restaurante));// crio o vetor de restauanrete
+        if(novaCole->restaurante == NULL){// verifico se a alocacao deu certo
+            printf("Erro ao alocar restaurante!");
+            return NULL;// se nao der retorna null
+        }
+        ler_csv_colecao(novaCole,"/tmp/restaurantes.csv");// chamada da funcao para registra restaurante
+
+      return novaCole;// retorno uma nova colecao
+}
+
+int buscar_id(Colecao_Restaurante* colecao, int id_buscado) {// funcao para buscar o restaurante
+    for (int i = 0; i < colecao->tamanho; i++) {//pego o tamanho da colecao
+        if (colecao->restaurante[i].id_restaurante == id_buscado) {// e procuro o id buscado do restaurante
+            return i; // retorno a posicao
+        }
+    }
+    return -1;// se nao encontra retorna -1
+}
+
+int transformarInt(char *s){// funcao para transforma o char em int
+    int qtdCaracteres;
+    for(qtdCaracteres=0; s[qtdCaracteres] != '\0' && s[qtdCaracteres] != '\n'; qtdCaracteres++);
+
+    int contador = 1;
+    int resposta = 0;
+    for(int i=qtdCaracteres-1; i>=0; i--) {
+        resposta += (s[i] - '0') * contador;//pega o valor e multiplica pela casa unidade decimal ou centena
+        contador *= 10;//aumento o valor do contador por exemplo 10, 100, 1000
+    }
+    return resposta;// retorno o valor do id
+}
+
+void swap(Restaurante *i, Restaurante *j, int* mov) {
+   Restaurante temp = *i;
+   *i = *j;
+   *j = temp;
+   (*mov) += 3;
+}
+
+void Selecao(Restaurante r[], int n, int* comp, int* mov){
+    
+    for(int i = 0; i < n - 1; i++){
+        int menor = i;
+        for(int j = i + 1; j < n; j++){
+            (*comp)++;
+            if(strcmp(r[j].nome, r[menor].nome) < 0){
+                menor = j;
+            }
+        }
+        swap(&r[i], &r[menor], mov);
+    }
+}
+int main(){
+   /*pequeno teste para ver se esta funcionando
+    Data d = parse_data("2026-04-13");
+    char b[11];
+    formatar_data(&d, b);
+    printf("%s ", b);
+    Hora h = parse_hora("12:00");
+    char s[6];
+    formatar_hora(&h, s);
+    printf("%s\n", s);*/
+    //ver o timer 
+    clock_t inicio, fim;
+    double total_tempo;
+
+    //crio a colecao
+    Colecao_Restaurante* cr = ler_csv();
+    
+    //Criando um array de restaurantes ordenados, e um int para saber os ordenados
+    Restaurante r_ordenados[1000];
+    int ordenados = 0;
+    int comp = 0, mov = 0;
+    char linha[5];
+    scanf("%s", linha);//leio a linha
+    while(strcmp(linha, "-1") != 0){//comparo se é diferente de -1
+        int id = transformarInt(linha);//transformo o valor
+
+        int id_buscado = buscar_id(cr, id);// busca o id na lista
+        if(id_buscado != -1){//verifico se é diferete de -1
+           r_ordenados[ordenados] = cr->restaurante[id_buscado];  
+           ordenados++;
+        }
+        scanf("%s", linha);// scan para a proxima linha
+    }
+    
+    //inicio do clock + selecao
+    inicio = clock();
+    Selecao(r_ordenados, ordenados, &comp, &mov);
+    fim = clock();
+    total_tempo = ((fim - inicio) / (double)CLOCKS_PER_SEC); 
+
+    for(int i = 0; i < ordenados; i++) {
+        char leitura[300];
+        formatar_restaurante(&r_ordenados[i], leitura);
+        printf("%s\n", leitura);
+    }
+
+    FILE* arq_log = fopen("880222_selecao.txt", "w");
+    
+    if(arq_log != NULL){
+        fprintf(arq_log, "880222\tComparacoes: %d\tMovimentos: %d\tTempo: %.2lf\n", comp, mov, total_tempo);
+        fclose(arq_log);
+    }
+
+
+    for (int i = 0; i < cr->tamanho; i++) {
+        liberar_restaurante(&cr->restaurante[i]);//libero os vetores criado de cada posicao
+    }
+    free(cr->restaurante);//libero o vetor de colecao restaurante
+
+    free(cr);//libero a colecao
+}
